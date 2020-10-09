55bbe0222e0df40ee10d77045bd06bcd29a2e472
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-08 20:47:29 -0500

    Tabs 💊

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const menu = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('is-active');
 });
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((item) => item.classList.remove('is-active'));
+    tab.classList.add('is-active');
+    const target = tab.dataset.target;
+    tabContentBoxes.forEach((box) => {
+      if (box.getAttribute('id') === target) {
+        box.classList.remove('is-hidden');
+      } else {
+        box.classList.add('is-hidden');
+      }
+    });
+  });
+});
